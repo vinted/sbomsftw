e48fde5f284c7b2116d9c805ec35b075c1dec908
Ugnius Vaznys <ugnius.vaznys@vinted.com>
2022-09-08 12:32:17 +0300

    Update GitHub actions
    Signed-off-by: Ugnius Vaznys <ugnius.vaznys@vinted.com>

--- build/push-to-cloudsmith.sh
+++ build/push-to-cloudsmith.sh
@@ -3,4 +3,5 @@ releaseVersion=$(echo "$RELEASE" | tr '/' -)
 releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH.tar.gz"
 go build -ldflags "-s -w" -o bin && tar -czvf "$releaseName" bin
 
-# cloudsmith push raw vinted/raw-hosted-security "$releaseName"
+pip3 install cloudsmith-cli
+cloudsmith push raw vinted/raw-hosted-security "$releaseName"
