578e8872cd65d765638cf540abf73ef4045627a5
Ugnius Vaznys <ugnius.vaznys@vinted.com>
2022-09-26 11:26:44 +0300

    Remove tar.gz packaging when pushing to cloudsmith
    Signed-off-by: Ugnius Vaznys <ugnius.vaznys@vinted.com>

--- build/push-to-cloudsmith.sh
+++ build/push-to-cloudsmith.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 releaseVersion=$(echo "$RELEASE" | tr '/' -)
-releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH.tar.gz"
-go build -ldflags "-s -w" -o sa-collector && tar -czvf "$releaseName" sa-collector
+releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH"
+
+go build -ldflags "-s -w" -o "$releaseName"
 
 pip3 install cloudsmith-cli
 cloudsmith push raw vinted/raw-hosted-security "$releaseName"
