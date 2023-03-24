c56bd80304846d2e5a7077698c85d89210bba0c1
Ugnius Vaznys <ugnius.vaznys@vinted.com>
2023-03-24 11:47:43 +0200

    Hide cloudsmith repository name in secrets
    Signed-off-by: Ugnius Vaznys <ugnius.vaznys@vinted.com>

--- build/push-to-cloudsmith.sh
+++ build/push-to-cloudsmith.sh
@@ -5,4 +5,4 @@ releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH"
 go build -ldflags "-s -w" -o "$releaseName"
 
 pip3 install cloudsmith-cli
-cloudsmith push raw vinted/raw-hosted-security "$releaseName"
+cloudsmith push raw "$CS_REPOSITORY" "$releaseName"
