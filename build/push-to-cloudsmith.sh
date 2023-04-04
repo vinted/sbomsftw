10c7e97d7b5c320e09a5b16d89b5ac5953f3c6df
Ugnius Vaznys <ugnius.vaznys@vinted.com>
2023-04-04 16:22:24 +0300

    Disable CGO when compiling
    Disable CGO when compiling to make the binary more portable.

--- build/push-to-cloudsmith.sh
+++ build/push-to-cloudsmith.sh
@@ -2,7 +2,7 @@
 releaseVersion=$(echo "$RELEASE" | tr '/' -)
 releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH"
 
-go build -ldflags "-s -w" -o "$releaseName"
+CGO_ENABLED=0 go build -ldflags "-s -w" -o "$releaseName"
 
 pip3 install cloudsmith-cli
 cloudsmith push raw "$CS_REPOSITORY" "$releaseName"
