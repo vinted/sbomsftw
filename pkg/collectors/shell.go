63fb1d6f0129e565492fcc74bf7108c53ceae06e
Vadimas Vasiljevas <vadimas.vasiljevas@vinted.com>
2025-04-15 11:08:22 +0300

    lets see if we can pid kill shellout

--- pkg/collectors/shell.go
+++ pkg/collectors/shell.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 	"time"
 
 	cdx "github.com/CycloneDX/cyclonedx-go"
@@ -58,27 +59,60 @@ func (d defaultShellExecutor) bomFromCdxgen(ctx context.Context, bomRoot string,
 	)
 	// Fetching licenses can time out so add a cancellation of 15 minutes
 	withLicensesCtx, withLicensesCancel := context.WithTimeout(ctx, withLicensesTimeout)
+	defer withLicensesCancel()
+
 	cdxGenCmd := formatCDXGenCmd(multiModuleMode, true, language, outputFile)
-	cmd := exec.CommandContext(withLicensesCtx, "bash", "-c", cdxGenCmd) //nolint:gosec
+	cmd := exec.CommandContext(withLicensesCtx, "bash", "-c", cdxGenCmd)
 	cmd.Dir = bomRoot
 
-	if err = cmd.Run(); err != nil {
-		withLicensesCancel()
+	// Set process group for better process management
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	pgid := int(0) // Will store process group ID for cleanup
+
+	// Run the command and get the process group ID
+	if err = cmd.Start(); err == nil {
+		pgid = cmd.Process.Pid // Save PID for process group termination
+		err = cmd.Wait()
+	}
+
+	// Ensure any spawned processes are terminated
+	if pgid > 0 {
+		// Kill the entire process group
+		_ = syscall.Kill(-pgid, syscall.SIGKILL)
+	}
+
+	// If first attempt failed, try without licenses
+	if err != nil {
 		log.WithError(err).Debugf("cdxgen failed - regenerating SBOMs without licensing info")
+
 		withoutLicensesCtx, withoutLicensesCancel := context.WithTimeout(ctx, withoutLicensesTimeout)
+		defer withoutLicensesCancel()
+
 		cdxGenCmd = formatCDXGenCmd(multiModuleMode, false, language, outputFile)
-		cmd = exec.CommandContext(withoutLicensesCtx, "bash", "-c", cdxGenCmd) //nolint:gosec
+		cmd = exec.CommandContext(withoutLicensesCtx, "bash", "-c", cdxGenCmd)
 		cmd.Dir = bomRoot
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
-		if err = cmd.Run(); err != nil {
-			withoutLicensesCancel()
+		pgid = 0 // Reset process group ID
 
+		if err = cmd.Start(); err == nil {
+			pgid = cmd.Process.Pid
+			err = cmd.Wait()
+		}
+
+		// Ensure any spawned processes are terminated
+		if pgid > 0 {
+			// Kill the entire process group
+			_ = syscall.Kill(-pgid, syscall.SIGKILL)
+		}
+
+		if err != nil {
 			return nil, fmt.Errorf("can't Collect SBOMs for %s: %v", bomRoot, err)
 		}
-		withoutLicensesCancel()
 	}
-	withLicensesCancel()
 
+	// Read and process output
 	output, err := os.ReadFile(outputFile)
 	if err != nil || len(output) == 0 {
 		return nil, fmt.Errorf("can't Collect %s SBOMs for %s", language, bomRoot)
