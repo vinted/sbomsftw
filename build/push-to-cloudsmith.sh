c5343722ccc29eb564629705b420273f3adfa0d4
Vadimas Vasiljevas <vadimas.vasiljevas@vinted.com>
2025-07-31 13:51:22 +0300

    update cloudsmith script build

--- build/push-to-cloudsmith.sh
+++ build/push-to-cloudsmith.sh
@@ -2,7 +2,7 @@
 releaseVersion=$(echo "$RELEASE" | tr '/' -)
 releaseName="sa-collector-$releaseVersion-$GOOS-$GOARCH"
 
-CGO_ENABLED=1 go build -ldflags "-s -w" -o "$releaseName"
+CGO_ENABLED=1 go build -tags "sqlite_json sqlite_fts5 sqlite_math_functions sqlite3" -ldflags "-s -w" -o "$releaseName"
 
 pip3 install cloudsmith-cli
 cloudsmith push raw "$CS_REPOSITORY" "$releaseName"
