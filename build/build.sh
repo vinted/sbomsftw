b8cd1eff122fad5514aecb87560d9d2c43af6858
Vadimas Vasiljevas <vadimas.vasiljevas@vinted.com>
2025-07-31 15:58:58 +0300

    update builder

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CGO_ENABLED=1 go build -tags "sqlite_json sqlite_fts5 sqlite_math_functions sqlite3" -ldflags "-s -w" -o sa-collector
+CGO_ENABLED=0 go build -tags "sqlite_json sqlite_fts5 sqlite_math_functions sqlite3" -ldflags "-s -w" -o sa-collector
